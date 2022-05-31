e83bf4ef402300cbb2dc19d5ae196dbcb21b09d4
kames <kamescg@gmail.com>
2022-05-31 08:26:16 -0600

    update(cli.js): fix network env references

--- src/cli.ts
+++ src/cli.ts
@@ -4,9 +4,9 @@ if (require.main === module) {
   const {
     RELAYER_API_KEY,
     RELAYER_API_SECRET,
-    ETHEREUM_MAINNET_PROVIDER_URL: ethereumRinkebyProviderURL,
-    POLYGON_MAINNET_PROVIDER_URL: polygonMumbaiProviderURL,
-    AVALANCHE_MAINNET_PROVIDER_URL: avalancheFujiProviderURL
+    ETHEREUM_RINKEBY_PROVIDER_URL: ethereumRinkebyProviderURL,
+    POLYGON_MUMBAI_PROVIDER_URL: polygonMumbaiProviderURL,
+    AVALANCHE_FUJI_PROVIDER_URL: avalancheFujiProviderURL
   } = process.env;
   handler({
     apiKey: RELAYER_API_KEY,
